eb0e68c7ac6ba14be059801ceec70d56f45414fc
apextor <44752313+apextor@users.noreply.github.com>
2018-11-07 02:53:03 +0530

    Update edit-footer-credits.php

--- edit-footer-credits.php
+++ edit-footer-credits.php
@@ -1,14 +1,10 @@
 <?php
 
-add_filter('genesis_footer_creds_text', 'conditional_footer_creds_filter');
-function conditional_footer_creds_filter( $creds ) {
-if ( is_front_page() ) {
-	$creds = '&copy; 2018 &middot; <a href="http://mydomain.com">My Custom Link</a> &middot; Built on the <a href="http://www.studiopress.com/themes/genesis" title="Genesis Framework">Genesis Framework</a>';
-	return $creds;
-	} else {
-	$creds = sprintf( '<p>%s<span class="dashicons dashicons-heart"></span>%s<a href="http://www.studiopress.com/">%s</a></p>',  __( 'Handcrafted with ', 'filter' ), __( ' on the', 'filter' ), __( ' Genesis Framework', 'filter' ) );
-	return $creds;
-	}
+//Changing the Credits
+function wpb_footer_creds_text () {
+	echo '<div class="creds"><p>Copyright &copy;';
+	echo date('Y');
+	echo ' &middot; <a href="https://wpbeaches.com/">Neil Gee</a> - All Rights Are Reserved &middot; Powered by<a href="https://wpbeaches.com"> WP Beaches</a></p></div>';
 }
 
 
